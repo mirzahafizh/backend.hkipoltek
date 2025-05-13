14ec706b3362c3a8bc9d1cf3734581e46fb4888d
mirzahaifzh <mirzafio78@gmail.com>
2025-05-13 16:59:21 +0800

    fix vercel.json

--- config/config.js
+++ config/config.js
@@ -1,23 +1,25 @@
+require('dotenv').config(); // Memuat file .env
+
 module.exports = {
   development: {
-    username: "root",
-    password: "root",
-    database: "HKI_POLTEKBA",
-    host: "127.0.0.1",
-    dialect: "mysql"
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT
   },
   test: {
-    username: "root",
-    password: "root",
-    database: "HKI_POLTEKBA",
-    host: "127.0.0.1",
-    dialect: "mysql"
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT
   },
   production: {
-    username: "root",
-    password: "root",
-    database: "HKI_POLTEKBA",
-    host: "127.0.0.1",
-    dialect: "mysql"
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT
   }
 };
